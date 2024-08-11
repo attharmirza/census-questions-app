73b5358375dacf9d764164a8a120fc17eb633425
Atthar Mirza <atthar.mirza@gmail.com>
2024-08-11 03:19:09 -0400

    Setting Up as Basic React App (#1)
    * setting up basic react app
    
    * changing default setup to something i like better
    
    * Started scaffolding app
    
    * renamed commands to match api
    
    * got css modules working

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 Use generative AI to answer questions about the United States with data from the U.S. Census Bureau.
 
 # Quick Start
-
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Input from './components/Input'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <Input />
+    </>
+  )
+}
