08b4086943e3407a7a892bddf7ae68e33eb4bae1
ADRIAN BLANCO <adrianblancoramos@gmail.com>
2024-08-11 14:18:23 -0400

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Header from './components/Header'
 import Input from './components/Input'
+
 import './App.css'
 
 export default function App() {
   return (
     <>
+      <Header/>
       <Input />
     </>
   )
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import styles from './index.module.css'
+
+export default function Header() {
+    return (
+        <>
+            <h1 className={styles.container}>
+            Census questions
+            </h1>
+        </>
+    )
+}
